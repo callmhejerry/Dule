a55199e417a7b1713ecfbd15fe5623109fd34428
Chinedu Jeremiah <chinedujeremiah2002@gmail.com>
2023-01-31 18:58:13 +0100

    added the calendar for the schedule screen

--- lib/schedule_screen.dart
+++ lib/schedule_screen.dart
@@ -1,9 +1,19 @@
 import 'package:dule/constants/screen_constants.dart';
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
-class ScheduleScreen extends StatelessWidget {
+class ScheduleScreen extends StatefulWidget {
   const ScheduleScreen({super.key});
 
+  @override
+  State<ScheduleScreen> createState() => _ScheduleScreenState();
+}
+
+class _ScheduleScreenState extends State<ScheduleScreen> {
+  final _focusedDay = DateTime.now();
+  final _firstDay = DateTime(2020);
+  final _lastDay = DateTime(2030);
+  final _calendarFormat = CalendarFormat.week;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,8 +24,37 @@ class ScheduleScreen extends StatelessWidget {
         ),
         child: SafeArea(
           child: Column(
-            children: const [
-              ScheduleHeader(),
+            children: [
+              const SizedBox(height: 10),
+              const ScheduleHeader(),
+              const SizedBox(
+                height: ScreenConstants.verticalSpacing,
+              ),
+              TableCalendar(
+                focusedDay: _focusedDay,
+                firstDay: _firstDay,
+                lastDay: _lastDay,
+                calendarFormat: _calendarFormat,
+                headerVisible: false,
+                calendarStyle: const CalendarStyle(),
+                currentDay: DateTime.now(),
+                startingDayOfWeek: StartingDayOfWeek.monday,
+                rowHeight: 50,
+                daysOfWeekStyle: const DaysOfWeekStyle(
+                  weekdayStyle: TextStyle(
+                    color: Color.fromARGB(255, 92, 92, 92),
+                    fontSize: 14,
+                  ),
+                  weekendStyle: TextStyle(
+                    fontSize: 14,
+                    color: Colors.grey,
+                  ),
+                ),
+                weekendDays: const [
+                  DateTime.saturday,
+                  DateTime.sunday,
+                ],
+              ),
             ],
           ),
         ),
@@ -31,11 +70,41 @@ class ScheduleHeader extends StatelessWidget {
   Widget build(BuildContext context) {
     final todaysDate = DateTime.now();
     return Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
       children: [
-        const Icon(Icons.calendar_today),
+        const Icon(
+          Icons.calendar_today,
+          color: Colors.grey,
+        ),
+        const SizedBox(
+          width: 12,
+        ),
         Text(
           todaysDate.toDays(),
+          style: const TextStyle(
+            color: Colors.deepPurpleAccent,
+            fontSize: 22,
+            fontWeight: FontWeight.bold,
+          ),
         ),
+        const SizedBox(
+          width: 5,
+        ),
+        Text(
+          todaysDate.year.toString(),
+          style: const TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.w400,
+          ),
+        ),
+        const Spacer(),
+        const Text(
+          "Today",
+          style: TextStyle(
+            color: Colors.deepPurpleAccent,
+            fontWeight: FontWeight.w500,
+          ),
+        )
       ],
     );
   }
